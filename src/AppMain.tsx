bf5bf5b98419a78d95842b4369b630375ce40faf
furuTra <tktk3008take@gmail.com>
2024-06-26 17:04:14 +0000

    XR機能を追加

--- src/AppMain.tsx
+++ src/AppMain.tsx
@@ -6,6 +6,7 @@ import { OrbitControls } from "@react-three/drei";
 import SelectCube from "./components/cube/SelectCube";
 import Polyhedron from "./components/polyhedron/Polyhedron";
 import styles from "./components/cube/Cube.module.css";
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
 
 export default function AppMain() {
   const polyhedron = useMemo(
@@ -37,26 +38,29 @@ export default function AppMain() {
   const cA = useControls("Cube A", cubeOptions);
   return (
     <div className={styles.cube__app}>
+      <ARButton />
       <Canvas camera={{ position: [1, 2, 3] }}>
-        <directionalLight position={[1, 1, 1]} intensity={0.8} />
-        <Polyhedron
-          position={[-1, 1, 0]}
-          rotation={[pA.x, pA.y, pA.z]}
-          visible={pA.visible}
-          color={pA.color}
-          polyhedron={polyhedron}
-        />
-        <Polyhedron
-          position={[1, 1, 0]}
-          rotation={[pB.x, pB.y, pB.z]}
-          visible={pB.visible}
-          color={pB.color}
-          polyhedron={polyhedron}
-        />
-        <OrbitControls target-y={1} />
-        <SelectCube position={[cA.posX, cA.posY, cA.posZ]} />
-        <axesHelper args={[5]} />
-        <gridHelper />
+        <XR>
+          <directionalLight position={[1, 1, 1]} intensity={0.8} />
+          <Polyhedron
+            position={[-1, 1, 0]}
+            rotation={[pA.x, pA.y, pA.z]}
+            visible={pA.visible}
+            color={pA.color}
+            polyhedron={polyhedron}
+          />
+          <Polyhedron
+            position={[1, 1, 0]}
+            rotation={[pB.x, pB.y, pB.z]}
+            visible={pB.visible}
+            color={pB.color}
+            polyhedron={polyhedron}
+          />
+          <OrbitControls target-y={1} />
+          <SelectCube position={[cA.posX, cA.posY, cA.posZ]} />
+          <axesHelper args={[5]} />
+          <gridHelper />
+        </XR>
       </Canvas>
     </div>
   );
